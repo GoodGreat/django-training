39bb7e6e9d7f5cd6c04e156ab5a36cf0456b4b2d
Alvaro <alvaro96rojo@gmail.com>
2022-03-21 10:16:53 +0100

    First commit, couple of steps into the tutorial

---
+++ polls/views.py
@@ -0,0 +1,12 @@
+"""
+This is the views.py file
+"""
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
