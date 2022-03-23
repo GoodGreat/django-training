e6df9acb47008e9feebc5cf9d1d0d4cdf1c4d5af
Alvaro <alvaro96rojo@gmail.com>
2022-03-23 14:29:50 +0100

    Add templates and update index view

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,23 @@
 """
 This is the views.py file
 """
-
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request) -> HttpResponse:
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id) -> HttpResponse:
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id) -> HttpResponse:
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % question_id)
